d96fe96fd220780da55745209a3d09b26ee95bf0
ReFantasy <309741309@qq.com>
2021-06-27 15:43:58 +0800

    .

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -1,9 +1,13 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include <QApplication>
+#include <QDebug>
+#include <QSslSocket>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug()<<QSslSocket::supportsSsl();
+    qDebug()<<QSslSocket::sslLibraryBuildVersionString();
     Widget w;
     w.show();
     return a.exec();
