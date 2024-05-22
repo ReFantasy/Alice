7e949418d7793ac8eee2decaf0f5599adb8018c6
refantasy-debian <309741309@qq.com>
2024-05-22 19:47:38 +0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "globalapp.h"
 
 const QString WINDOW_TITLE("Alice Translation");
 
 int main(int argc, char *argv[])
 {
-    // QApplication app(argc, argv);
-    globalApp app(argc, argv);
+    QApplication app(argc, argv);
 
     MainWindow main_window;
     main_window.setWindowTitle(WINDOW_TITLE);
