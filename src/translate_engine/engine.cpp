26d788fec3fa053fd32d854e2ed482010096110a
Refantasy-debian <309741309@qq.com>
2024-08-12 09:57:28 +0800

    .

--- src/translate_engine/engine.cpp
+++ src/translate_engine/engine.cpp
@@ -16,7 +16,7 @@ bool TranslateEngine::SetIdKey(QString eng_id, QString eng_key)
 }
 
 //----------------------------------------------------------------------------------------
-//    
+//
 //----------------------------------------------------------------------------------------
 
 bool BaiduEngine::SetIdKey(QString eng_id, QString eng_key)
@@ -26,24 +26,20 @@ bool BaiduEngine::SetIdKey(QString eng_id, QString eng_key)
 
 QString BaiduEngine::WrapperAPIRequest(QString from, QString to, QString src_text)
 {
-    auto from_byte = from.toUtf8();
-    auto to_byte = to.toUtf8();
     auto query_byte = src_text.toUtf8();
     auto appid_byte = _id.toUtf8();
     auto key_byte = _key.toUtf8();
-
     auto salt_byte = QString::number(RandomUInt()).toUtf8();
 
     auto step1_byte = appid_byte + query_byte + salt_byte + key_byte;
 
     auto sign_byte = QCryptographicHash::hash(step1_byte, QCryptographicHash::Md5).toHex();
 
-    // QString http = QString("http://api.fanyi.baidu.com/api/trans/vip/translate");
-    QString http = QString("https://fanyi-api.baidu.com/api/trans/vip/translate");
-    QString params = QString("?q=%1&from=%2&to=%3&appid=%4&salt=%5&sign=%6")
-                         .arg(query_byte.toPercentEncoding(), from_byte, to_byte, appid_byte, salt_byte, sign_byte);
+    QString params =
+        QString("?q=%1&from=%2&to=%3&appid=%4&salt=%5&sign=%6")
+            .arg(query_byte.toPercentEncoding(), from.toUtf8(), to.toUtf8(), appid_byte, salt_byte, sign_byte);
 
-    return http + params;
+    return QString("https://fanyi-api.baidu.com/api/trans/vip/translate") + params;
 }
 
 QString BaiduEngine::ParseHttpReply(QString http_reply)
